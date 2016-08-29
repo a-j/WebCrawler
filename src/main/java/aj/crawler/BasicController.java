893d1669760aa229fb0e0c66265290afec05f4d5
Anand Jayabalan <anand.jbn@gmail.com>
2016-08-29 15:54:04 -0400

    added logback dependency, log statements and redirected crawler output to file system

--- src/main/java/aj/crawler/BasicController.java
+++ src/main/java/aj/crawler/BasicController.java
@@ -8,27 +8,22 @@ import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
 
 public class BasicController {
     public static void main(String[] args) throws Exception {
-        String crawlStorageFolder = "~/IdeaProjects/WebCrawler/data";
-        int numberOfCrawlers = 7;
+        String crawlStorageFolder = "data"; // tmp folder for use during crawling
+        int numberOfCrawlers = 1;
 
         CrawlConfig config = new CrawlConfig();
         config.setCrawlStorageFolder(crawlStorageFolder);
         config.setPolitenessDelay(400);
         config.setMaxDepthOfCrawling(1);
-//        config.setMaxPagesToFetch(-1);
 
         PageFetcher pageFetcher = new PageFetcher(config);
         RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
         RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
         CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
 
-        controller.addSeed("https://www.optum.com/");
+        controller.addSeed("http://www.optum.com/");
 
         controller.startNonBlocking(BasicCrawler.class, numberOfCrawlers);
-
-        //Thread.sleep(300 * 1000);
-
-        //controller.shutdown();
         controller.waitUntilFinish();
     }
 }
--- src/main/java/aj/crawler/BasicCrawler.java
+++ src/main/java/aj/crawler/BasicCrawler.java
@@ -5,6 +5,8 @@ import edu.uci.ics.crawler4j.crawler.WebCrawler;
 import edu.uci.ics.crawler4j.parser.HtmlParseData;
 import edu.uci.ics.crawler4j.url.WebURL;
 
+import java.io.*;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.regex.Pattern;
 
@@ -13,33 +15,75 @@ import java.util.regex.Pattern;
  */
 public class BasicCrawler extends WebCrawler {
 
-    private static final Pattern FILTERS = Pattern.compile(".*(\\.(css|js|gif|jpg|png|bmp))$");
-    public static int i = 1;
+    private static final Pattern FILTERS = Pattern.compile(".*(\\.(css|js|gif|jpg|jpeg|png|bmp|ico))$");
+    Set<String> internalLinks = new HashSet<>();
+    Set<String> externalLinks = new HashSet<>();
 
     @Override
     public boolean shouldVisit(Page referringPage, WebURL url) {
         String href = url.getURL().toLowerCase();
 
+        System.out.println("***** href: " + href);
         if (FILTERS.matcher(href).matches()) {
             return false;
         }
+        System.out.println("***** shouldVisit() called *****");
 
         return href.contains("www.optum.com/");
-//        return true;
     }
 
     @Override
     public void visit(Page page) {
         String url = page.getWebURL().getURL();
-        System.out.println(i++ + ") URL: " + url);
+        System.out.println("URL: " + url);
+        System.out.println("Domain: " + page.getWebURL().getDomain());
 
         if (page.getParseData() instanceof HtmlParseData) {
             HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
-            String text = htmlParseData.getText();
-            String html = htmlParseData.getHtml();
             Set<WebURL> links = htmlParseData.getOutgoingUrls();
 
             System.out.println("Number of outgoing links: " + links.size());
+            System.out.println("Outgoing links: " + links);
+            processUrls(links, page.getWebURL());
+            System.out.println("Number of internal links: " + internalLinks.size());
+            System.out.println("Number of external links: " + externalLinks.size());
+            System.out.println("Internal links: " + internalLinks);
+            System.out.println("External links: " + externalLinks);
+        }
+    }
+
+    public void processUrls(Set<WebURL> urls, WebURL pageUrl) {
+        for (WebURL url : urls) {
+            if (url.getDomain().equals(pageUrl.getDomain())) {
+                internalLinks.add(url.getDomain());
+            } else {
+                externalLinks.add(url.getDomain());
+            }
+        }
+    }
+
+    @Override
+    public void onBeforeExit() {
+        System.out.println("Number of internal links: " + internalLinks.size());
+        System.out.println("Number of external links: " + externalLinks.size());
+        try {
+            FileWriter fileWriter = new FileWriter(new File("data/output-" + getThread().getName() + ".txt"));
+
+            fileWriter.write("Internal Links:\n");
+            for (String url : internalLinks) {
+                fileWriter.write(url);
+                fileWriter.write("\n");
+            }
+            fileWriter.write("\n");
+
+            fileWriter.write("External Links:\n");
+            for (String url : externalLinks) {
+                fileWriter.write(url);
+                fileWriter.write("\n");
+            }
+        } catch (IOException ioe) {
+            System.out.println("IOException Occurred");
+            ioe.printStackTrace();
         }
     }
 }
