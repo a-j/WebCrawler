d607590c5f69e47d1f3728a63b50e13389471e0f
Anand Jayabalan <anand.jbn@gmail.com>
2016-08-30 16:14:52 -0400

    clean up - switched to logger instead of s.o.p. and added execution time

--- src/main/java/aj/crawler/BasicController.java
+++ src/main/java/aj/crawler/BasicController.java
@@ -6,6 +6,8 @@ import edu.uci.ics.crawler4j.fetcher.PageFetcher;
 import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
 import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
 
+import java.time.LocalDate;
+
 public class BasicController {
     public static void main(String[] args) throws Exception {
         String crawlStorageFolder = "data"; // tmp folder for use during crawling
@@ -14,16 +16,19 @@ public class BasicController {
         CrawlConfig config = new CrawlConfig();
         config.setCrawlStorageFolder(crawlStorageFolder);
         config.setPolitenessDelay(400);
-        config.setMaxDepthOfCrawling(1);
+        config.setMaxDepthOfCrawling(2);
 
         PageFetcher pageFetcher = new PageFetcher(config);
         RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
         RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
         CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
 
-        controller.addSeed("http://www.optum.com/");
+        controller.addSeed("http://www.optum.com");
 
+        final long startTime = System.currentTimeMillis();
         controller.startNonBlocking(BasicCrawler.class, numberOfCrawlers);
         controller.waitUntilFinish();
+        final long endTime = System.currentTimeMillis();
+        System.out.println("Elapsed Time (in seconds): " + (endTime - startTime)/1000 );
     }
 }
--- src/main/java/aj/crawler/BasicCrawler.java
+++ src/main/java/aj/crawler/BasicCrawler.java
@@ -4,6 +4,8 @@ import edu.uci.ics.crawler4j.crawler.Page;
 import edu.uci.ics.crawler4j.crawler.WebCrawler;
 import edu.uci.ics.crawler4j.parser.HtmlParseData;
 import edu.uci.ics.crawler4j.url.WebURL;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.util.HashSet;
@@ -15,44 +17,37 @@ import java.util.regex.Pattern;
  */
 public class BasicCrawler extends WebCrawler {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(BasicCrawler.class);
     private static final Pattern FILTERS = Pattern.compile(".*(\\.(css|js|gif|jpg|jpeg|png|bmp|ico))$");
-    Set<String> internalLinks = new HashSet<>();
-    Set<String> externalLinks = new HashSet<>();
+    private Set<String> internalLinks = new HashSet<>();
+    private Set<String> externalLinks = new HashSet<>();
 
     @Override
     public boolean shouldVisit(Page referringPage, WebURL url) {
         String href = url.getURL().toLowerCase();
 
-        System.out.println("***** href: " + href);
         if (FILTERS.matcher(href).matches()) {
             return false;
         }
-        System.out.println("***** shouldVisit() called *****");
+        LOGGER.debug("Current url:{} Domain: {} ", url.getURL(), url.getDomain());
+        LOGGER.debug("Parent url:{} Domain: {} ", referringPage.getWebURL().getURL(), referringPage.getWebURL().getDomain());
 
-        return href.contains("www.optum.com/");
+        return url.getDomain().equals(referringPage.getWebURL().getDomain());
     }
 
     @Override
     public void visit(Page page) {
         String url = page.getWebURL().getURL();
-        System.out.println("URL: " + url);
-        System.out.println("Domain: " + page.getWebURL().getDomain());
+        LOGGER.info("Visiting URL: {}", url);
 
         if (page.getParseData() instanceof HtmlParseData) {
             HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
             Set<WebURL> links = htmlParseData.getOutgoingUrls();
-
-            System.out.println("Number of outgoing links: " + links.size());
-            System.out.println("Outgoing links: " + links);
             processUrls(links, page.getWebURL());
-            System.out.println("Number of internal links: " + internalLinks.size());
-            System.out.println("Number of external links: " + externalLinks.size());
-            System.out.println("Internal links: " + internalLinks);
-            System.out.println("External links: " + externalLinks);
         }
     }
 
-    public void processUrls(Set<WebURL> urls, WebURL pageUrl) {
+    private void processUrls(Set<WebURL> urls, WebURL pageUrl) {
         for (WebURL url : urls) {
             if (url.getDomain().equals(pageUrl.getDomain())) {
                 internalLinks.add(url.getDomain());
@@ -64,8 +59,8 @@ public class BasicCrawler extends WebCrawler {
 
     @Override
     public void onBeforeExit() {
-        System.out.println("Number of internal links: " + internalLinks.size());
-        System.out.println("Number of external links: " + externalLinks.size());
+        LOGGER.info("Number of internal links: {}", internalLinks.size());
+        LOGGER.info("Number of external links: {}", externalLinks.size());
         try {
             FileWriter fileWriter = new FileWriter(new File("data/output-" + getThread().getName() + ".txt"));
 
@@ -81,9 +76,10 @@ public class BasicCrawler extends WebCrawler {
                 fileWriter.write(url);
                 fileWriter.write("\n");
             }
+            fileWriter.flush();
+            fileWriter.close();
         } catch (IOException ioe) {
-            System.out.println("IOException Occurred");
-            ioe.printStackTrace();
+            LOGGER.error("IOException Occurred", ioe);
         }
     }
 }
